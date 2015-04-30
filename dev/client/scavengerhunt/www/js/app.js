7ccd38410d5116366f13e34f1551bf8a1458510a
riahk <moar.riah@gmail.com>
2015-04-30 16:59:14 -0700

    add test photos to photofeed

--- dev/client/scavengerhunt/www/js/app.js
+++ dev/client/scavengerhunt/www/js/app.js
@@ -10,12 +10,15 @@ angular.module('scavengerhunt', ['ionic'])
   $stateProvider.state('home', {
     url: '/',
     templateUrl: 'hunts.html',
+    controller: 'HuntsCtrl'
   })
 
   .state('pics', {
     url: 'pics',
-    templateUrl: 'pics.html'
+    templateUrl: 'pics.html',
+    controller: 'PhotosCtrl'
   })
+
 })
 .controller('AppCtrl', function($ionicModal, $scope) {
   $ionicModal.fromTemplateUrl('huntsmodal.html', {
@@ -34,6 +37,50 @@ angular.module('scavengerhunt', ['ionic'])
     $scope.modal.remove();
   });
   
+})
+.controller('HuntsCtrl', function($scope) {
+  $scope.test = function() {
+    console.log('hello!');
+  }
+})
+.controller('PhotosCtrl', function($scope, $ionicModal) {
+  $scope.photos = [
+    {
+      src: "img/noah.jpg",
+      lat: 37.783482,
+      lon: -122.409116,
+      comment: "It's Noah!!! Nice hat."
+    },
+    {
+      src: "img/building.jpg",
+      lat: 37.783602,
+      lon: -122.409360,
+      comment: "cool building."
+    }
+  ];
+  $ionicModal.fromTemplateUrl('picInfo.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.selectedPhoto = null;
+
+  $scope.openModal = function(photo) {
+    $scope.selectedPhoto = photo;
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  $scope.test = function() {
+    console.log(this.selectedPhoto);
+  }
 })
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
